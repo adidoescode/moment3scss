867f04a660431234b692925b80739878e2168a8c
Adam Sjögren <adidoescode@gmail.com>
2024-04-09 15:39:40 +0200

    Har gjort klart layouten för 501px och under och ska pusha till mitt repo för att se så att allt fungerar på offentliga webbläsare.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("Hejsan Svejsan");
+let openBtn = document.getElementById("open-menu");
+let closeBtn = document.getElementById("close-menu");
+let navMenuEl = document.getElementById("nav-menu");
+
+openBtn.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+
+
+    if (navMenuEl.style.display === "none" || navMenuEl.style.display === "") {
+        navMenuEl.style.display = "block";
+    }
+    else {
+        navMenuEl.style.display = "none";
+    }
+}
